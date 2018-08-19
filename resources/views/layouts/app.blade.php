9504487c0b62041eaf712d8c34dae3e0d359d29d
gakkispy <yaosenjun168@live.cn>
2018-08-19 06:00:13 +0000

    Finish basic page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>@yield('title', 'LaravBBS') - Laravel 进阶教程</title>
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        
+        @include('layouts._header')
+
+        <div class="container">
+
+            @yield('content')
+
+        </div>
+
+        @include('layouts._footer')
+    </div>
+
+    <!-- Script -->
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
