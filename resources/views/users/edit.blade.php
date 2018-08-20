4ad9edbf569afa05ed87f7b9e67abbad39c0fef9
gakkispy <yaosenjun168@live.cn>
2018-08-20 03:42:08 +0000

    basic user profile editing

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<div class="container">
+    <div class="panel panel-default col-md-10 col-md-offset-1">
+        <div class="panel-heading">
+            <h4>
+                <i class="glyphicon glyphicon-edit"></i> 编辑个人资料
+            </h4>
+        </div>
+
+        @include('common.error')
+
+        <div class="panel-body">
+
+            <form action="{{ route('users.update', $user->id) }}" accept-charset="UTF-8" method="post" class="form">
+                {{ csrf_field() }}
+                {{ method_field('PUT') }}
+
+                <div class="form-group">
+                    <label for="name-field" class="control-label">用户名</label>
+                    <input type="text" class="form-control" name="name" id="name-field" value="{{ old('name', $user->name) }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="email-field" class="control-label">E-Mail</label>
+                    <input type="email" class="form-control" name="email" id="email-field" value="{{ old('email', $user->email) }}">
+                </div>
+
+                <div class="form-group">
+                    <label for="introduction-field" class="control-label">个人简介</label>
+                    <textarea  class="form-control" name="introduction" id="introduction-field" rows="3" >{{ old('introduction', $user->introduction) }}</textarea>
+                </div>
+
+                <div class="well well-sm">
+                    <button type="submit" class="btn btn-primary">保存</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -16,10 +16,10 @@
                         <div class="media-body">
                             <hr>
                             <h4><strong>个人简介</strong></h4>
-                            <p>Despesido</p>
+                            <p>{{ $user->introduction }}</p>
                             <hr>
                             <h4><strong>注册于</strong></h4>
-                            <p>1999.9.1</p>
+                            <p>{{ $user->created_at->diffForHumans() }}</p>
                         </div>
                     </div>
                 </div>
