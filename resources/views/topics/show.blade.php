fa38463296a679b487f564d4665fe3e32cbf5407
gakkispy <yaosenjun168@live.cn>
2018-08-23 16:21:30 +0000

    List user replies

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -6,60 +6,70 @@
 @section('content')
 
 <div class="container">
-    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
-        <div class="panel panel-default">
-
-            <div class="panel-body">
-                <div class="text-center">
-                    作者： {{ $topic->user->name }}
-                </div>
-
-                <hr>
-                <div class="media">
-                    <div align="center">
-                        <a href="{{ route('users.show', $topic->user->id) }}">
-                            <img src="{{ $topic->user->avatar }}" alt="{[ $topic->user->name }}" width="300px" height="300px" class="thumbnail img-responsive">
-                        </a>
+    <div class="row">       
+        <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
+            <div class="panel panel-default">
+    
+                <div class="panel-body">
+                    <div class="text-center">
+                        作者： {{ $topic->user->name }}
+                    </div>
+    
+                    <hr>
+                    <div class="media">
+                        <div align="center">
+                            <a href="{{ route('users.show', $topic->user->id) }}">
+                                <img src="{{ $topic->user->avatar }}" alt="{[ $topic->user->name }}" width="300px" height="300px" class="thumbnail img-responsive">
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-        <div class="panel panel-default">
+    
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
             <div class="panel panel-default">
-                <div class="panel-body">
-                    <h1 class="text-center">
-                        {{ $topic->title }}
-                    </h1>
-
-                    <div class="article-meta text-center">
-                        {{ $topic->created_at->diffForHumans() }}
-                        ·
-                        <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
-                        {{ $topic->reply_count }}
-                    </div>
-
-                    <div class="topic-body">
-                        {!! $topic->body !!}
-                    </div>
-
-                    @can('update',$topic)
-                        <div class="operate">
-                            <hr>
-                            <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs pull-left" role="button">
-                                <i class="glyphicon glyphicon-edit"></i> 编辑
-                            </a>
-                            <form action="{{ route('topics.destroy', $topic->id) }}" method="post">
-                                {{ csrf_field() }}
-                                {{ method_field('DELETE') }}
-                                <button class="btn btn-default btn-xs pull-left"  style="margin-left: 6px;">
-                                    <i class="glyphicon glyphicon-trash"></i> 删除
-                                </button>
-                            </form>
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <h1 class="text-center">
+                            {{ $topic->title }}
+                        </h1>
+    
+                        <div class="article-meta text-center">
+                            {{ $topic->created_at->diffForHumans() }}
+                            ·
+                            <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+                            {{ $topic->reply_count }}
                         </div>
-                    @endcan
+    
+                        <div class="topic-body">
+                            {!! $topic->body !!}
+                        </div>
+    
+                        @can('update',$topic)
+                            <div class="operate">
+                                <hr>
+                                <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs pull-left" role="button">
+                                    <i class="glyphicon glyphicon-edit"></i> 编辑
+                                </a>
+                                <form action="{{ route('topics.destroy', $topic->id) }}" method="post">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button class="btn btn-default btn-xs pull-left"  style="margin-left: 6px;">
+                                        <i class="glyphicon glyphicon-trash"></i> 删除
+                                    </button>
+                                </form>
+                            </div>
+                        @endcan
+                    </div>
+                </div>
+            </div>
+
+            {{--  用户回复列表  --}}
+            <div class="panel panel-default topic-reply">
+                <div class="panel-body">
+                    @include('topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
                 </div>
             </div>
         </div>
