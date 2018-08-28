c2093a6b18189fa791d509190857bda18e6b001d
gakkispy <yaosenjun168@live.cn>
2018-08-28 09:27:32 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,13 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        // 这样写扩展性更高，只有空的时候才指定默认头像
+
+        if (empty($user->avatar)) {
+            $user->avatar = 'http://wx4.sinaimg.cn/large/86883a42gy1frwn5l7700j20cu09r751.jpg';
+        }
+    }
 }
