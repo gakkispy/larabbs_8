b0dff31daafd70d3cf83284c1a0ff385d9e8396d
gakkispy <yaosenjun168@live.cn>
2018-09-04 06:19:40 +0000

    User update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Cache;
 use App\Models\User;
 
 use App\Transformers\UserTransformer;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -45,5 +46,22 @@ class UsersController extends Controller
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
     }
 }
