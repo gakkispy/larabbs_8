8f9db10fcf24a25b3c90770cfed2de3901066092
gakkispy <yaosenjun168@live.cn>
2018-09-05 10:41:17 +0000

    notifications index

---
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Transformers\NotificationTransformer;
+
+class NotificationsController extends Controller
+{
+    public function index()
+    {
+        $notifiations = $this->user->notifications()->paginate(20);
+
+        return $this->response->paginator($notifiations, new NotificationTransformer());
+    }    
+}
